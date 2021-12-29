90765ca8906f732202a5581ce8b37f8ee75af9c0
IsisG13 <isissouzaguimaraes@gmail.com>
2021-12-29 12:49:38 -0300

    mudar nome

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,14 +4,13 @@ import github from "./git.png";
 function Footer() {
   return (
     <footer className={style.container}>
-      <p>Desenvolvido por: Marcelly Guimarães. </p>
+      <p>Desenvolvido por: Marcelly Guimarães. ✨</p>
       <img src={github} alt="github" className={style.logo} />
       <a
         href="https://github.com/MarcellyGuimaraes"
         target="_blank"
         rel="noopener noreferrer"
       >
-        {" "}
         Meu Github
       </a>
     </footer>
